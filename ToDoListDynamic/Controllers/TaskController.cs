2e89a5f61891239d81d556aa4fce680358f4ead2
Matthew Kovalyk <matviigame@gmail.com>
2023-11-15 19:23:58 +0200

    Add project files.

---
+++ ToDoListDynamic/Controllers/TaskController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoListDynamic.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            var tasksCookie = Request.Cookies["tasks"];
+            var tasks = tasksCookie != null ? tasksCookie.Split(',') : Array.Empty<string>();
+
+            return View("Index", tasks);
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(string taskName)
+        {
+            var tasksCookie = Request.Cookies["tasks"];
+
+            tasksCookie = string.IsNullOrEmpty(tasksCookie) ? taskName : $"{tasksCookie},{taskName}";
+
+            Response.Cookies.Append("tasks", tasksCookie, new CookieOptions
+            {
+                Expires = DateTimeOffset.Now.AddDays(7)
+            });
+
+            return Json(new { success = true });
+        }
+
+        [HttpPost]
+        public IActionResult DeleteTask()
+        {
+            Response.Cookies.Delete("tasks");
+
+            return Json(new { success = true });
+        }
+    }
+}
